672fe5899dba63c497dd483e26aa62fb4d02c284
jesiok <38736868+jesiok@users.noreply.github.com>
2022-12-10 09:43:15 +0100

    Update cls_db.py

--- cls_db.py
+++ cls_db.py
@@ -2,9 +2,13 @@ import sqlite3
 import pandas as pd
 from pandas.core.frame import DataFrame
 
+
 # c:/Python/BED/example.sqlite
 # wszedzie dodawc poprze args, tak bezpieczniej i baardziej prawidlowo
+
 class DB_class():
+# INICJALIZACJA: 
+# db = DB_class(db='browser_ads.db')
     
 # TO DO, przy SQLu dodać param    
     def __init__(self,db):
@@ -14,26 +18,34 @@ class DB_class():
     # def __enter__(self):
     #     return self
 
-    # select
-
-    def select_pandas(self,sql):
+## select
+    def select_to_df_by_pandas(self,sql):
+        # Przykladowe zastosowanie:
+        # db = DB_class(db='DB_browser_adv.db')
+        # df = db.select_to_df("select * from tbl_browser_adv_summary")
         df = pd.read_sql_query(sql, self.connection)
         return df
 
-    def select_fetchall_to_df(self,sql,args):
+    def select_to_df_by_fetchall(self,sql,args):
         self.cursor.execute(sql,args)
         c = self.cursor.fetchall()
         df = pd.DataFrame(c)
         return df
         # info fetchone - zwraca jedna linijka, jako tuple, fetchall zwraca liste
 
-    def select_fetchall(self,sql,args):
+    def select_by_fetchall(self,sql,args):
         self.cursor.execute(sql)
         c = self.cursor.fetchall()
         return c
-        
-    def insert(self,sql,args):
-        self.cursor.execute(sql,args)
+
+## insert     
+
+    def insert(self,sql):
+        # Przykladowe zastosowanie:
+        # db = DB_class(db='DB_browser_adv.db')
+        # db.insert("INSERT INTO tbl_browser_adv_summary (TASK, WEBSITE) VALUES ('test3','interia_test')")
+        # df = db.select_to_df_by_pandas("select * from tbl_browser_adv_summary")
+        self.cursor.execute(sql)
         id = self.cursor.lastrowid
         self.connection.commit()
         return id  
@@ -43,26 +55,23 @@ class DB_class():
         rowcount = self.cursor.rowcount
         self.connection.commit()
         return rowcount   
-    
+
+## update    
     def update(self,sql,args):
-        self.cursor.executemany(sql)
+        self.cursor.execute(sql)
         rowcount = self.cursor.rowcount
         self.connection.commit()
         return rowcount  
-        
+
+## delete        
     def delete(self):
-        pass    
-        
+        pass   
+     
+## drop    
     def drop(self):
         pass 
 
-    def __del__(self):
-        if self.connection != None:
-            self.connection.close()
-        #if self.cursor != None:
-         #   self.cursor.close()
-    
-        # create
+## create
     def create(self,db_name,sql):
         # sql = f"""CREATE TABLE IF NOT EXISTS {db_name} (
         #     "ID"	INTEGER,
@@ -85,3 +94,14 @@ class DB_class():
 # __repr__
 # __new__
 
+    def __del__(self):
+        if self.connection != None:
+            self.connection.close()
+        #if self.cursor != None:
+         #   self.cursor.close()
+
+## tests
+# db = DB_class(db='DB_browser_adv.db')
+# db.insert("INSERT INTO tbl_browser_adv_summary (TASK, WEBSITE) VALUES ('test3','interia_test')")
+# df = db.select_to_df_by_pandas("select * from tbl_browser_adv_summary")
+# print(df)
